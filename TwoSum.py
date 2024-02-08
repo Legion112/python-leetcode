4a0554084f402d9bc84f6a58dfc9799472c3a900
legion112 <ageev.maks2014@gmail.com>
2024-02-08 14:59:39 +0400

    Converted test to pytest

--- TwoSum.py
+++ TwoSum.py
@@ -3,10 +3,9 @@ from typing import List, Optional, Dict
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> Optional[List[int]]:
-        hashmap: Dict[int:int] = {value: key for key, value in enumerate(nums)}
+        hashmap: Dict[int, int] = {value: key for key, value in enumerate(nums)}
         for firstIndex, firstValue in enumerate(nums):
-            second_index: Optional[int] = hashmap.get(target - firstValue)
-            if second_index is not None and second_index != firstIndex:
-                return [firstIndex, second_index]
-        print("No value to return")
+            secondIndex: Optional[int] = hashmap.get(target - firstValue)
+            if secondIndex is not None and secondIndex != firstIndex:
+                return [firstIndex, secondIndex]
         return None
--- test_TwoSum.py
+++ test_TwoSum.py
@@ -1,26 +1,18 @@
-import unittest
-
+import pytest
 from TwoSum import Solution
 
+@pytest.mark.parametrize("nums,target,expected", [
+    ([2, 7, 11, 15], 9, [0, 1]),
+    ([3, 2, 4], 6, [1, 2]),
+    ([2, 7, 11, 15], 9, [0, 1]),
+    ([3, 3], 6, [0, 1]),
+])
+def test_two_sum(nums, target, expected):
+    solution = Solution()
+    result = solution.twoSum(nums, target)
+    assert result == expected, f"Failed for nums: {nums} with target: {target}. Expected: {expected}, got: {result}"
 
-class TestSolution(unittest.TestCase):
-    def test_two_sum(self):
-        test_cases = [
-            ([2, 7, 11, 15], 9, [0, 1]),
-            ([3, 2, 4], 6, [1, 2]),
-            ([3, 3], 6, [0, 1]),
-        ]
-        solution = Solution()
-        for nums, target, expected in test_cases:
-            with self.subTest(nums=nums, target=target, expected=expected):
-                result = solution.twoSum(nums, target)
-                self.assertListEqual(
-                    result,
-                    expected,
-                 f"Failed for nums: {nums} with target: {target}. Expected: {expected}, got: {result}"
-                )
-
-    def test_two_sum_no_return_value(self):
-        solution = Solution()
-        result = solution.twoSum([3, 3], 0)
-        self.assertIsNone(result, f"Expected: None, got: {result}")
+def test_two_sum_no_return_value():
+    solution = Solution()
+    result = solution.twoSum([3, 3], 0)
+    assert result is None, f"Expected: None, got: {result}"
