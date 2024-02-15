009c6f1288ec1c5b761abf100664608dff5b7d92
legion112 <ageev.maks2014+git@gmail.com>
2024-02-15 17:59:44 +0300

    Add 100% coverage

--- test_PalindromeNumber.py
+++ test_PalindromeNumber.py
@@ -5,6 +5,7 @@ from PalindromeNumber import Solution
 
 @pytest.mark.parametrize("number,expected", [
     (121, True),  # Example 1
+    (123, False),  # Example 1
 ])
 def test_solution(number: int, expected: bool):
     solution = Solution()
