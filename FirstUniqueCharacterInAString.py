0c7dd08d85a9f835e040069ba75ce5f6405bce71
legion112 <ageev.maks2014+git@gmail.com>
2024-02-15 21:07:37 +0300

    Easy to read sollution

--- FirstUniqueCharacterInAString.py
+++ FirstUniqueCharacterInAString.py
@@ -1,20 +1,11 @@
+import collections
 from typing import Dict, Set
 
 
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        seenTwice: Set[str] = set()
-        seenOne: Dict[str, int] = dict()
-        for pos, char in enumerate(s):
-            if char in seenTwice:
-                continue
-            if char in seenOne:
-                seenOne.pop(char)
-                seenTwice.add(char)
-                continue
-            seenOne[char] = pos
-        for pos in seenOne.values():
-            return pos
-
+        count: Dict[str, int] = collections.Counter(s)
+        for idx, ch in enumerate(s):
+            if count[ch] == 1:
+                return idx
         return -1
-
